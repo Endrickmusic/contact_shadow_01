7f0eebc75747790207dff9813a63292b91718df8
Christian <mail@christianhohenbild.de>
2024-01-19 02:16:59 +0100

    add contact shadow

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Logo from '/face-blowing-a-kiss.svg'
 import { Canvas } from "@react-three/fiber";
-import { Environment } from "@react-three/drei";
+import { Environment, ContactShadows } from "@react-three/drei";
 import Experience from "./Experience";
 import './index.css'
 
@@ -12,13 +12,24 @@ export default function App() {
  return (
 
   
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 40 }}>
+    <Canvas 
+    shadows 
+    camera={{ position: [3, 3, 3], fov: 40 }}
+    >
       <Environment
         files="./Environments/envmap.hdr" />
         <color 
           attach="background" 
           args={["#c1efef"]} />
       <Experience />
+      <ContactShadows
+          position={[0, -1.5, 0]}
+          width={5}
+          height={5}
+          far={10}
+          rotation={[0, 0, 0]}
+          frames={1}
+        />
     </Canvas>
   
   );
